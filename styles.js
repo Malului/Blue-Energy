af52563bd0e143683f6315bdb23cc23c1dba8ccc
Malului Mutua <malului227@gmail.com>
2024-10-19 12:15:29 +0300

    initial commit

---
+++ styles.js
@@ -0,0 +1,13 @@
+
+
+        function getConfirmation(){ retVal = confirm("Do you want to continue ?");
+        if (retVal == true) {
+            document.write(<a href="Blue Energies.html"/>);
+            return true;
+            
+        } else {
+            document.write("User does not want to continue!");
+            return false;
+        }
+    }
+ //-->
